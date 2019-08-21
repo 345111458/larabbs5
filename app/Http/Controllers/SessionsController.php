2ca63994e925418c17b22237c5170452998d3556
vagrant <vagrant@homestead>
2019-08-21 03:13:15 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -35,6 +35,14 @@ class SessionsController extends Controller
     }
 
 
+    public function destroy(){
+
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect()->route('login');
+    }
+
+
 
 
     //
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -43,5 +43,7 @@ class UsersController extends Controller
 
 
 
+
+
     //
 }
